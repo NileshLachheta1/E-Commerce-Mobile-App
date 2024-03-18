b3b3dfa1dcd840ce72a9d34052d845a50f02f09d
xyz <xyz@gmail.com>
2024-03-18 22:53:49 +0530

    First Commit 18-March-2024

---
+++ api/index.js
@@ -0,0 +1,193 @@
+import express from 'express';
+import cors from 'cors';
+import bodyparser from 'body-parser';
+import mongoose from 'mongoose';
+import nodemailer from 'nodemailer';
+import jwt from 'jsonwebtoken';
+import crypto, {generateKey} from 'crypto';
+import User from './models/user.js';
+import Order from './models/order.js';
+import { request } from 'http';
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+mongoose
+  .connect(
+    'mongodb+srv://nileshlachheta1995:aaccihID0AiXQFxT@cluster0.nhpfyuq.mongodb.net/ECommerce',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  )
+  .then(() => {
+    console.log('Database Connection Successful');
+  })
+  .catch(err => {
+    console.log('Error While Database Connection', err);
+  });
+
+app.listen(port, () => {
+  console.log('Server Running at ', port);
+});
+
+// endpoint to register in the app
+
+// function to send Verification Email to the user
+const sendVerificationEmail = async (email, verificationToken) => {
+  //  create a nodemailer transport
+  const transporter = nodemailer.createTransport({
+    // configure the email service
+    service: 'gmail',
+    auth: {
+      user: 'nileshlachheta1995@gmail.com',
+      pass: 'mydlzncehglllxkf',
+    },
+  });
+
+  // compose the email message
+  const mailOption = {
+    from: 'nileshlachheta1995@gmail.com',
+    to: email,
+    subject: 'Email Verification',
+    text: `Please click the following link to verify your E-mail : http://localhost:8000/verify/${verificationToken}`,
+  };
+
+  // send email
+  try {
+    await transporter.sendMail(mailOption);
+  } catch (error) {
+    console.log('Error sending the verification E-mail', error);
+  }
+};
+
+app.post('/register', async (request, response) => {
+  try {
+    const {name, email, password} = request.body;
+
+    // check email is already register
+    const existingUser = await User.findOne({email});
+
+    if (existingUser) {
+      return response.status(400).json({message: 'E-mail already registered'});
+    } else {
+      // Create a new user
+      const newUser = new User({name, email, password});
+
+      // generate and store the verification token
+      newUser.verificationToken = crypto.randomBytes(20).toString('hex');
+
+      // save the user to the database
+      await newUser.save();
+
+      // send verification email to the user
+      sendVerificationEmail(newUser.email, newUser.verificationToken);
+    }
+  } catch (err) {
+    console.log('Error While Registration', err);
+    response.status(500).json({message: 'Registration Failed'});
+  }
+});
+
+// endpoint to verify the email
+app.get('/verify/:token', async (request, response) => {
+  try {
+    const token = request.params.token;
+
+    // find the user with the given verification token
+    const user = await User.findOne({verificationToken: token});
+
+    if (!user) {
+      return response.status(404).json({message: 'Invalid Verification token'});
+    }
+
+    // mark the user as verified
+    user.verified = true;
+    user.verificationToken = undefined;
+    await user.save();
+    response.status(200).json({message: 'E-mail Verified Successfully'});
+  } catch (error) {
+    response.status(500).json({message: 'Email Verification Failed'});
+  }
+});
+
+const generateSecreyKey = () => {
+  const secretkey = crypto.randomBytes(32).toString('hex');
+  return secretkey;
+};
+
+const secretkey = generateSecreyKey();
+
+// end point to login the user
+
+app.post('/login', async (request, response) => {
+  try {
+    const {email, password} = request.body;
+
+    // check if the user exists
+    const user = await User.findOne({email});
+
+    if (!user) {
+      return response.status(401).json({message: 'invalid email or password'});
+    }
+
+    // check if the password is correct
+    if (user.password !== password) {
+      return response.status(401).json({message: 'invalid password'});
+    }
+
+    // generate a token
+
+    const token = jwt.sign({userId: user._id}, secretkey);
+
+    response.status(200).json({token});
+  } catch (error) {
+    // console.log(error);
+    response.status(500).json({message: 'Login Failed'});
+  }
+});
+
+// endpoint to store a new address to the backend
+app.post("/address", async (request,response)=>{
+  try {
+    const{userId, address} = request.body;
+    console.log("inside backend : ",request.body);
+    // find the user by UserId
+    const user = await User.findById(userId);
+    
+    if(!user){
+      return response.status(404).json({message:"User not found"});
+    }
+
+    // add the new address to the user's addresses array
+    user.addresses.push(address);
+
+    // save the updated user in the backend
+    await user.save();
+    response.status(200).json({message:"Address Created Successfully"});
+
+  } catch (error) {
+    response.status(500).json({message:"Error adding address"});
+  }
+}) 
+
+
+//  endpoint to get all the addresses of a particular user
+app.get("/addresses/:userId", async(request,response)=>{
+  try {
+    const userId = request.params.userId;
+
+    const user = await User.findById(userId);
+    if(!user){
+      return response.status(404).json({message:"user not found"});
+    }
+
+    const addresses  = user.addresses;
+    response.status(200).json({addresses})
+  } catch (error) {
+    response.status(500).json({message : "error retriving the addresses"})
+  }
+})
---
+++ screens/AddAddressScreen.js
@@ -0,0 +1,195 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  TextInput,
+} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {useNavigation} from '@react-navigation/native';
+import axios from 'axios';
+import {UserType} from '../UserContext';
+const AddAddressScreen = () => {
+  const navigation = useNavigation();
+  const [addresses, setAddresses] = useState([]);
+  const {userId, setUserId} = useContext(UserType);
+  // setUserId("65f7e36e92127dee17308a7d");
+  console.log('userId', userId);
+  useEffect(() => {
+    fetchAddresses();
+  }, []);
+
+  const fetchAddresses = async () => {
+    try {
+      const response = await axios.get(
+        `http://192.168.1.14:8000/addresses/65f7e36e92127dee17308a7d`,
+      );
+      const addresses = response.data.addresses;
+      setAddresses(addresses);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log('addresses', addresses);
+  return (
+    <ScrollView showsVerticalScrollIndicator={false} style={{marginTop: 50}}>
+      <View
+        style={{
+          backgroundColor: '#00CED1',
+          padding: 10,
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <Pressable
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginHorizontal: 7,
+            gap: 10,
+            backgroundColor: 'white',
+            borderRadius: 3,
+            height: 38,
+            flex: 1,
+          }}>
+          <AntDesign
+            style={{paddingLeft: 10}}
+            name="search1"
+            size={22}
+            color="#000000"
+          />
+          <TextInput placeholder="Search Amazon.in"></TextInput>
+        </Pressable>
+        <Feather
+          style={{paddingLeft: 10}}
+          name="mic"
+          size={22}
+          color="#000000"
+        />
+      </View>
+      <View style={{padding: 10}}>
+        <Text style={{fontSize: 22, fontWeight: 'bold', color: 'black'}}>
+          Your Addresses
+        </Text>
+
+        <Pressable
+          onPress={() => navigation.navigate('Add')}
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            marginTop: 10,
+            borderColor: '#D0D0D0',
+            borderWidth: 2,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            paddingVertical: 7,
+            paddingHorizontal: 5,
+          }}>
+          <Text style={{color: 'black'}}>Add a new Address</Text>
+          <MaterialIcons
+            style={{paddingLeft: 10}}
+            name="keyboard-arrow-right"
+            size={24}
+            color="#000000"
+          />
+        </Pressable>
+        <Pressable>
+          {/* all the added addresses */}
+          {addresses.map((item, index) => {
+            return (
+              <Pressable
+                style={{
+                  borderWidth: 1,
+                  borderColor: '#D0D0D0',
+                  padding: 10,
+                  flexDirection: 'column',
+                  gap: 5,
+                  marginVertical: 10,
+                }}>
+                <View
+                  style={{flexDirection: 'row', alignItems: 'center', gap: 3}}>
+                  <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+                    {item.name}
+                  </Text>
+                  <Entypo
+                    style={{paddingLeft: 10}}
+                    name="location-pin"
+                    size={24}
+                    color="red"
+                  />
+                </View>
+                <Text style={{fontSize: 15, color: '#181818'}}>
+                  {item.houseNo},{item.landmark}
+                </Text>
+                <Text style={{fontSize: 15, color: '#181818'}}>
+                  {item.street}
+                </Text>
+                <Text style={{fontSize: 15, color: '#181818'}}>
+                  India, Banglore
+                </Text>
+                <Text style={{fontSize: 15, color: '#181818'}}>
+                  Phone No. {item.mobileNo}
+                </Text>
+                <Text style={{fontSize: 15, color: '#181818'}}>
+                  Pin Code {item.postalCode}
+                </Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 10,
+                    marginTop: 10,
+                  }}>
+                  <Pressable
+                    style={{
+                      backgroundColor: '#F5F5F5',
+                      paddingHorizontal: 10,
+                      paddingVertical: 6,
+                      borderRadius: 5,
+                      borderWidth: 1,
+                      borderColor: '#D0D0D0',
+                    }}>
+                    <Text>Edit</Text>
+                  </Pressable>
+
+                  <Pressable
+                    style={{
+                      backgroundColor: '#F5F5F5',
+                      paddingHorizontal: 10,
+                      paddingVertical: 6,
+                      borderRadius: 5,
+                      borderWidth: 1,
+                      borderColor: '#D0D0D0',
+                    }}>
+                    <Text>Remove</Text>
+                  </Pressable>
+
+                  <Pressable
+                    style={{
+                      backgroundColor: '#F5F5F5',
+                      paddingHorizontal: 10,
+                      paddingVertical: 6,
+                      borderRadius: 5,
+                      borderWidth: 1,
+                      borderColor: '#D0D0D0',
+                    }}>
+                    <Text>Set as Default</Text>
+                  </Pressable>
+                </View>
+              </Pressable>
+            );
+          })}
+        </Pressable>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default AddAddressScreen;
+
+const styles = StyleSheet.create({});
